6aeaa270e7d52921a021c8b93c4f76705ff49aad
rakib <rakibsarowar@gmail.com>
2023-04-07 17:26:46 +0600

    completed nested router and use loader to fetch data and also useLoaderData hook to get the fetch data

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,9 @@ import OrderReview from './components/OrderReview/OrderReview';
 // Step 05: our common section will be Header and after will use <Outlet> for route items.
 //Step06: Next we will set up children_ children:[{ }] in main jsx. and first child will be <Home> and path '/'
 //Step07: improve the Header with Nav manu by link & to. & after usuing css, carefull to import in jsx.
+// Step08: create data json file and fetch in it main jsx file by using loader: ()=>fetch('filename')
+// Step09: go to in Home jsx and useloaderdata() hook to get the data.
+
 
 const router = createBrowserRouter([
 {
@@ -24,7 +27,8 @@ const router = createBrowserRouter([
   children:[
     {
       path: '/',
-      element: <Home></Home>
+      element: <Home></Home>,
+      loader: ()=> fetch('tshirts.json')
     },
     {
       path: 'review',
