b0866e7bb57272afd7b5299cd4cbe7733b4315a3
rakib <rakibsarowar@gmail.com>
2023-04-07 17:12:07 +0600

    added nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/review">Order Review</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,29 @@ import {
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import Main from './components/Layout/Main';
+import OrderReview from './components/OrderReview/OrderReview';
 // Step 01: import 3 line in main jsx.
 // Step 02: create router and set basic path by creating in layout.|| Header, Home
 //Step 03: set <RouterProvider router={router} /> by replcing <App> || By this 3 steps we make a route.
 //Step04: crate laout in conponent which name can be, root, main, admin. Where 1/2 part will be common like header and other will be change able.
 // Step 05: our common section will be Header and after will use <Outlet> for route items.
+//Step06: Next we will set up children_ children:[{ }] in main jsx. and first child will be <Home> and path '/'
+//Step07: improve the Header with Nav manu by link & to.
 
 const router = createBrowserRouter([
 {
   path: '/',
-  element: <Main></Main>
+  element: <Main></Main>,
+  children:[
+    {
+      path: '/',
+      element: <Home></Home>
+    },
+    {
+      path: 'review',
+      element:<OrderReview></OrderReview>
+    }
+  ]
 }
 
 ])
