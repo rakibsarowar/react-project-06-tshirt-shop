e6ec6653bfeb71d013ddc9fe45ede3aae8e2b1d4
rakib <rakibsarowar@gmail.com>
2023-04-07 17:01:21 +0600

    added main layout

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './components/Home/Home';
+import Main from './components/Layout/Main';
 // Step 01: import 3 line in main jsx.
 // Step 02: create router and set basic path by creating in layout.|| Header, Home
 //Step 03: set <RouterProvider router={router} /> by replcing <App> || By this 3 steps we make a route.
-
+//Step04: crate laout in conponent which name can be, root, main, admin. Where 1/2 part will be common like header and other will be change able.
+// Step 05: our common section will be Header and after will use <Outlet> for route items.
 
 const router = createBrowserRouter([
 {
   path: '/',
-  element: <Home></Home>
+  element: <Main></Main>
 }
 
 ])
